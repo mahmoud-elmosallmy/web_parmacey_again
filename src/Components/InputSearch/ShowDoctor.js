c4562bab75223eb65fa3ec561ec31d22894066e6
Mahmoud <Mosallmym@gmail.com>
2023-12-02 14:21:50 +0200

    update page pharmacy V 3.1

--- src/Components/InputSearch/ShowDoctor.js
+++ src/Components/InputSearch/ShowDoctor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router';
 import styled from 'styled-components'
 import { useFilterDoctors } from '../../Context/DoctorsFilterContext';
